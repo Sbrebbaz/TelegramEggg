173147038ff93314b2b6b7b8a8ccb9463b0fc950
Davide Nembrini <davide.nembrini00@gmail.com>
2024-06-06 20:13:12 +0200

    Test bot to that converts text to percursor images

--- EggGeneratorTelegram/Worker.cs
+++ EggGeneratorTelegram/Worker.cs
@@ -1,19 +1,29 @@
-using EggGeneratorTelegram.Settings;
+﻿using EggGeneratorTelegram.Settings;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp.Drawing.Processing;
 
 namespace EggGeneratorTelegram
 {
 	public class Worker : BackgroundService
 	{
 		private TelegramBotClient _telegramClient;
+		private FontCollection _fontCollection = new FontCollection();
+		private FontFamily _fontfamily;
+		private Font _font;
 
 		public Worker()
 		{
 			_telegramClient = new TelegramBotClient(AppSettings.Current.TelegramToken);
+			_fontfamily = _fontCollection.Add("Resources/precursoralpha.ttf");
+			_font = _fontfamily.CreateFont(120, FontStyle.Regular);
 		}
 
 		public async override Task StartAsync(CancellationToken cancellationToken)
@@ -47,15 +57,55 @@ namespace EggGeneratorTelegram
 		private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 		{
 			long? chatID = update.Message?.Chat.Id;
+			string chatMessage = update.Message.Text ?? string.Empty;
 
 			if (chatID != null)
 			{
 				Console.WriteLine($"Received a message in chat {chatID}.");
 
-				Message sentMessage = await botClient.SendTextMessageAsync(
-					chatId: chatID,
-					text: $"EGG: {Guid.NewGuid()}",
-					cancellationToken: cancellationToken);
+				//Message sentMessage = await botClient.SendTextMessageAsync(
+				//	chatId: chatID,
+				//	text: chatMessage,
+				//	cancellationToken: cancellationToken);
+
+				TextOptions options = new TextOptions(_font)
+				{
+					Dpi = 72,
+					KerningMode = KerningMode.Auto
+				};
+
+				FontRectangle rect = TextMeasurer.MeasureAdvance(chatMessage, options);
+
+				using (Image image = new Image<Rgba32>((int)rect.Width, (int)rect.Height))
+				{
+					if (System.IO.File.Exists("test.bmp"))
+					{
+						System.IO.File.Delete("test.bmp");
+					}
+
+					image.Mutate(x => x.DrawText(
+						chatMessage,
+						_font,
+						new SixLabors.ImageSharp.Color(Rgba32.ParseHex("#FFFFFFEE")),
+						new PointF(image.Width - rect.Width,
+								image.Height - rect.Height)));
+
+					image.SaveAsBmp("test.bmp");
+
+					using (StreamReader streamReader = new StreamReader("test.bmp"))
+					{
+						await botClient.SendPhotoAsync(
+							chatId: chatID,
+							photo: InputFile.FromStream(streamReader.BaseStream),
+							caption: chatMessage,
+							cancellationToken: cancellationToken);
+					}
+
+					if (System.IO.File.Exists("test.bmp"))
+					{
+						System.IO.File.Delete("test.bmp");
+					}
+				}
 			}
 		}
 
